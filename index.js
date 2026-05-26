6931530f56fe535c25370781dbc3a0eaf6a75e78
Gunther Nettel Rosario <nettelgunther@outlook.es>
2026-05-26 23:55:44 +0300

    A new message even more friendly

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World! This is my first pipeline :D</h1>')
+  res.send('<h1>Hello World! This is my first pipeline :D</h1><br/> <p>What a wonderful day!</p>')
 })
 
 const PORT = 8080
